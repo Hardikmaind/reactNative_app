2842352d1b420abda225f4e446132b5d6911f898
Hardikmaind <maindhardik@gmail.com>
2025-01-08 15:19:44 +0530

    refactor: remove bottom tab layout and integrate stack navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
 export default function _layout() {
   return (
     <GestureHandlerRootView>
-      <Slot />
+      {/* <Slot /> */}
+{/*  //! IF WE USED THE Stack OR Tabs, we dont have to explicitely specify the <Slot/> */}
+      <Stack screenOptions={{headerShown:true}}/>          
+      {/* //! HERE ABOVE I HAVE USED THE PROP WHICH HIDES THE HEADER */}
     </GestureHandlerRootView>
   );
 }
